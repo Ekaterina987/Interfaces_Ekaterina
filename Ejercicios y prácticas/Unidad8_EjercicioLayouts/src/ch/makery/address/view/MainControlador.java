097ac555febd233d1090621de3ae3b8e1e388263
Ekaterina987 <91088904+Ekaterina987@users.noreply.github.com>
2022-02-22 13:02:42 +0100

    ejercicio2

--- Ejercicios y prácticas/Unidad8_EjercicioLayouts/src/ch/makery/address/view/MainControlador.java
+++ Ejercicios y prácticas/Unidad8_EjercicioLayouts/src/ch/makery/address/view/MainControlador.java
@@ -1,18 +1,46 @@
 package ch.makery.address.view;
 
+import java.util.List;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.cell.ComboBoxListCell;
 
 
 public class MainControlador {
 
 	@FXML
 	private ChoiceBox<String> locationBox;
+	
+	public static final ObservableList java = 
+	        FXCollections.observableArrayList();
+	
+	@FXML
+	//private ListView<String> qualificationsListView;
+	public static final ObservableList data = 
+    		FXCollections.observableArrayList();
 
 
     @FXML
     void initialize() {
     	locationBox.getItems().addAll("New York", "Orlando", "London", "Manchester"); 
+    	//for (int i = 0; i < 10; i++) {
+		//			qualificationsListView.getItems().add("Indeterminate (pick a choice)");
+		//	    }
+    	final ListView qualificationsListView = new ListView(data);
+    	java.addAll("Objects", "Classes", "Functions", "Variables", "Compiler", "Debugger", "Projects", "Beans", "Libraries", "Modules");
+    	for (int i = 0; i < 10; i++) {
+    		data.add("Indeterminate (pick a choice)");
+        }
+    	qualificationsListView.setItems(data);
+    	qualificationsListView.setCellFactory(ComboBoxListCell.forListView(java));
+    	
+    	qualificationsListView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+
     }
 
 }
--- Ejercicios y prácticas/Unidad8_EjercicioLayouts/src/module-info.java
+++ Ejercicios y prácticas/Unidad8_EjercicioLayouts/src/module-info.java
@@ -1,6 +1,7 @@
 module Unidad8_EjercicioLayouts {
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires javafx.base;
 	
 	opens ch.makery.address to javafx.graphics, javafx.fxml;
 	opens ch.makery.address.view to javafx.fxml;
