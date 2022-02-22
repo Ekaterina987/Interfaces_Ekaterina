de1d6dfb0d39effda6bda8578ebe8b91b80a446b
Ekaterina987 <91088904+Ekaterina987@users.noreply.github.com>
2022-02-22 13:20:06 +0100

    ejercicio2 update

--- Ejercicios y prácticas/Unidad8_EjercicioLayouts/src/ch/makery/address/view/MainControlador.java
+++ Ejercicios y prácticas/Unidad8_EjercicioLayouts/src/ch/makery/address/view/MainControlador.java
@@ -6,8 +6,10 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
 import javafx.scene.control.SelectionMode;
+import javafx.scene.control.Separator;
 import javafx.scene.control.cell.ComboBoxListCell;
 
 
@@ -23,11 +25,17 @@ public class MainControlador {
 	//private ListView<String> qualificationsListView;
 	public static final ObservableList data = 
     		FXCollections.observableArrayList();
+	@FXML
+	 private ComboBox<String> idiomaCombo;
 
 
     @FXML
     void initialize() {
-    	locationBox.getItems().addAll("New York", "Orlando", "London", "Manchester"); 
+    	Separator sep = new Separator();
+        sep.setMaxWidth(80);
+        //Adding separator to the choice box
+        ObservableList list = locationBox.getItems();
+    	list.addAll("New York", "Orlando", sep, "London", "Manchester"); 
     	//for (int i = 0; i < 10; i++) {
 		//			qualificationsListView.getItems().add("Indeterminate (pick a choice)");
 		//	    }
@@ -40,6 +48,8 @@ public class MainControlador {
     	qualificationsListView.setCellFactory(ComboBoxListCell.forListView(java));
     	
     	qualificationsListView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+    	
+    	idiomaCombo.getItems().addAll("English", "Japanese", "Spanish");
 
     }
 
