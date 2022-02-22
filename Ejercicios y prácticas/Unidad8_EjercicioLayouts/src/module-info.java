924a949378fe53ae8096a897521f5c9abb59de5b
Ekaterina987 <91088904+Ekaterina987@users.noreply.github.com>
2022-02-22 12:19:50 +0100

    version

--- Ejercicios y prácticas/Unidad8_EjercicioLayouts/src/module-info.java
+++ Ejercicios y prácticas/Unidad8_EjercicioLayouts/src/module-info.java
@@ -1,5 +1,6 @@
 module Unidad8_EjercicioLayouts {
 	requires javafx.controls;
+	requires javafx.fxml;
 	
-	opens application to javafx.graphics, javafx.fxml;
+	opens ch.makery.address to javafx.graphics, javafx.fxml;
 }
