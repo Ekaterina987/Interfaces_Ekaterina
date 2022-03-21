47a4518ac68d390b9fa622e61184232d62c59469
eka <ekastroev@gmail.com>
2022-03-21 19:24:18 +0100

    Tema 10

--- Ejercicios y prácticas/Unidad8_EjercicioLayouts/src/module-info.java
+++
@@ -1,9 +0,0 @@
-module Unidad8_EjercicioLayouts {
-	requires javafx.controls;
-	requires javafx.fxml;
-	requires javafx.base;
-	requires javafx.graphics;
-	
-	opens ch.makery.address to javafx.graphics, javafx.fxml;
-	opens ch.makery.address.view to javafx.fxml;
-}
