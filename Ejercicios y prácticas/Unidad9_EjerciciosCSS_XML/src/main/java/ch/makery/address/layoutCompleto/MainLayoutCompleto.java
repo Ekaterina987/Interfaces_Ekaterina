0d994d01213aa77f5b4046d68285d427b8dbe841
Ekaterina987 <91088904+Ekaterina987@users.noreply.github.com>
2022-04-03 23:56:57 +0200

    avance ejercicios

---
+++ Ejercicios y prácticas/Unidad9_EjerciciosCSS_XML/src/main/java/ch/makery/address/layoutCompleto/MainLayoutCompleto.java
@@ -0,0 +1,34 @@
+package ch.makery.address.layoutCompleto;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
+
+public class MainLayoutCompleto extends Application {
+	private BorderPane rootLayout;
+
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			// Carga el diseño del archivo FXML en la variable rootLayout
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainLayoutCompleto.class.getResource("view/LayoutCompletoView.fxml"));
+			rootLayout = (BorderPane) loader.load();
+
+			// Mostramos la escena del FlowPane de la variable rootLayot
+			Scene scene = new Scene(rootLayout);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
--- Ejercicios y prácticas/Unidad9_EjerciciosCSS_XML/src/main/java/module-info.java
+++ Ejercicios y prácticas/Unidad9_EjerciciosCSS_XML/src/main/java/module-info.java
@@ -3,6 +3,8 @@ module ch.makery.address {
     requires javafx.fxml;
 
 
-    opens ch.makery.address to javafx.fxml;
-    exports ch.makery.address;
+    opens ch.makery.address.flowpanecss to javafx.fxml;
+    opens ch.makery.address.layoutCompleto to javafx.fxml;
+    exports ch.makery.address.flowpanecss;
+    exports ch.makery.address.layoutCompleto;
 }
